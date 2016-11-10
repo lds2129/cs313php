b69dadf55999ddf271eecdc39b57dd8346733edf
James Kim <kim15012@byui.edu>
2016-11-10 08:35:23 -0700

    modified
    Signed-off-by: James Kim <kim15012@byui.edu>

---
+++ web/ultaree/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>discussion board</title>
+      <meta charset="UTF-8">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css' />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+        <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css' />
+        <link rel="stylesheet" type="text/css" href="../mystyle.css" />
+  </head>
+    
+  <body>   
+        <div id="mySidenav" class="sidenav">
+          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Clients</a>
+          <a href="#">Contact</a>
+        </div>
+
+        
+        <h1><center>Ultaree, BYUI Korean Association</center></h1>
+        <nav id="nav">
+            <ul id="nav">
+                <li><a href = "../main.html">Home</a></li>
+                <li><a href = "../assignments.html">Ultaree?</a></li>
+                <li><a href = "../discussion.php">Board!</a></li>
+                <li><a href = "../assignments.html">Activity!</a></li>
+                <li><a href = "../assignments.html">Gallery</a></li>
+                <li><a href = "index.php?action=signin">Log in</a></li>
+            </ul>                
+        </nav>
+    <div id="separator"></div>
+    <section id="mainSection">
